389ca8f034e9293d9c60dba2ca6e4574b0097cae
MrBreadWater <jpaniagua1541@gmail.com>
2018-07-14 11:25:02 -0700

    Fixed (hopefully) ALL indentation errors not previously visible in Notepad++

--- octoprint_kronos_data_collector/__init__.py
+++ octoprint_kronos_data_collector/__init__.py
@@ -24,7 +24,7 @@ class KronosDataCollector(octoprint.plugin.SettingsPlugin,
 		dict(type='settings', custom_bindings=False, template='fail_data_settings.jinja2')
 		]
 
-	def get_update_information(self):
+    def get_update_information(self):
         return dict(
             kronos_data_collector=dict(
                 displayName="Kronos Data Collector",
@@ -51,7 +51,7 @@ class KronosDataCollector(octoprint.plugin.SettingsPlugin,
             self.upload_timelapse(payload)
 		if event == Events.PRINT_CANCELLED
 			self.upload_picture()
-	def upload_picture(self):
+    def upload_picture(self):
 		if enablePlugin:
 			random_filename = str(''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])) + 'jpg'
 			urllib.urlretrieve ("http://localhost:8080/?action=snapshot", random_filename)
@@ -71,51 +71,51 @@ class KronosDataCollector(octoprint.plugin.SettingsPlugin,
             #self._logger.info('Deleted %s from local disk.' % file_name)
 
 
-	def upload_file(self, file, filename, pic = True)
+    def upload_file(self, file, filename, pic = True)
 		
-		self._logger.info('Uploading %s to S3 Server...' % file_name)
-		if pic = False:	
-			try:
+	self._logger.info('Uploading %s to S3 Server...' % file_name)
+	if pic = False:	
+		try:
 
-				conn = boto.s3.connect_to_region('us-west-1',
-				aws_access_key_id = 'AKIAJJ4BJOQSV5MGQHIQ',
-				aws_secret_access_key = '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',
-				# host = 's3-website-us-east-1.amazonaws.com',
-				# is_secure=True,               # uncomment if you are not using ssl
-				calling_format = boto.s3.connection.OrdinaryCallingFormat(),
-				)
+                        conn = boto.s3.connect_to_region('us-west-1',
+                        aws_access_key_id = 'AKIAJJ4BJOQSV5MGQHIQ',
+                        aws_secret_access_key = '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',
+                        # host = 's3-website-us-east-1.amazonaws.com',
+                        # is_secure=True,               # uncomment if you are not using ssl
+                        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+                        )
 
-				bucket = conn.get_bucket('3dprintdetectionuploads')
-				key_name = filename + '.mpg'
-				UpPath = 'prints/' #Directory Under which file should get upload
-				full_key_name = os.path.join(UpPath, key_name)
-				k = bucket.new_key(full_key_name)
-				k.set_contents_from_filename(file)
-				self._logger.info('Uploaded %s to S3 Server!' % file_name)
-			except Exception as e: 
-				self._logger.info(str(e))
-				self._logger.info("error")
-		elif pic = True:
-			try:
+                        bucket = conn.get_bucket('3dprintdetectionuploads')
+                        key_name = filename + '.mpg'
+                        UpPath = 'prints/' #Directory Under which file should get upload
+                        full_key_name = os.path.join(UpPath, key_name)
+                        k = bucket.new_key(full_key_name)
+                        k.set_contents_from_filename(file)
+                        self._logger.info('Uploaded %s to S3 Server!' % file_name)
+                except Exception as e: 
+                        self._logger.info(str(e))
+                        self._logger.info("error")
+        elif pic = True:
+                try:
 
-				conn = boto.s3.connect_to_region('us-west-1',
-				aws_access_key_id = 'AKIAJJ4BJOQSV5MGQHIQ',
-				aws_secret_access_key = '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',
-				# host = 's3-website-us-east-1.amazonaws.com',
-				# is_secure=True,               # uncomment if you are not using ssl
-				calling_format = boto.s3.connection.OrdinaryCallingFormat(),
-				)
+                        conn = boto.s3.connect_to_region('us-west-1',
+                        aws_access_key_id = 'AKIAJJ4BJOQSV5MGQHIQ',
+                        aws_secret_access_key = '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',
+                        # host = 's3-website-us-east-1.amazonaws.com',
+                        # is_secure=True,               # uncomment if you are not using ssl
+                        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+                        )
 
-				bucket = conn.get_bucket('3dprintdetectionuploads')
-				key_name = filename + '.jpg'
-				UpPath = 'print_pics/' #Directory Under which file should get upload
-				full_key_name = os.path.join(UpPath, key_name)
-				k = bucket.new_key(full_key_name)
-				k.set_contents_from_filename(key_name)
-				self._logger.info('Uploaded %s to S3 Server!' % file_name)
-			except Exception as e: 
-				self._logger.info(str(e))
-				self._logger.info("error")
+                        bucket = conn.get_bucket('3dprintdetectionuploads')
+                        key_name = filename + '.jpg'
+                        UpPath = 'print_pics/' #Directory Under which file should get upload
+                        full_key_name = os.path.join(UpPath, key_name)
+                        k = bucket.new_key(full_key_name)
+                        k.set_contents_from_filename(key_name)
+                        self._logger.info('Uploaded %s to S3 Server!' % file_name)
+                except Exception as e: 
+                        self._logger.info(str(e))
+                        self._logger.info("error")
 			
 __plugin_name__ = "Kronos Data Collector"
 
