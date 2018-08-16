7d0757f7d0e3c82982e4890fc712ec26283764cf
MrBreadWater <jpaniagua1541@gmail.com>
2018-08-16 13:43:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_kronos_data_collector"
 plugin_name = "Kronos Data Collector"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.1.3"
+plugin_version = "1.1.5"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/MrBreadWater/project-kronos-data-collector"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ['boto']
+plugin_requires = ['boto3']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
