fbc1567aa3fb39ecab88c43932108c6c84074dcc
Michael Paniagua <mrbreadwater@yahoo.com>
2020-04-25 21:05:43 -0700

    Update __init__.py

--- octoprint_kronos_data_collector/__init__.py
+++ octoprint_kronos_data_collector/__init__.py
@@ -73,25 +73,28 @@ class KronosDataCollector(octoprint.plugin.SettingsPlugin,
                 scram_s = 'aGsybVFET2JuaFhyR29PV3pLQ2NYTDBwdnNXM01qVmRBSW45QytDaQ=='
                 s3 = boto3.client('s3', aws_access_key_id=str(base64.b64decode(scram_a).decode()), aws_secret_access_key=str(base64.b64decode(scram_s).decode()))
                 bucket_name = 'kronos-plugin-uploads'
-                s3.upload_file(file, bucket_name, '%s' % ('print_pics/' if pic else 'prints/') + filename)
+                s3.upload_file(file, bucket_name, '%s' % ('print_pics/' if pic else 'prints/' + filename.split('/')[-1])
                 self._logger.info('Uploaded %s to S3 Server!' % ("photo" if pic else "timelapse"))
         except Exception as e:
                 self._logger.warn("Could not upload: %s" % e)
     def upload_picture(self):
         enablePlugin = self.enablePlugin
         if enablePlugin:
+            try:
                 snapshot_url = self._settings.global_get(["webcam", "snapshot"])
-                random_filename = str(''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])) + '.jpg'
+                random_filename = str('/tmp/'+''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])) + '.jpg'
                 urlrequest(snapshot_url, filename=random_filename)
                 self._logger.info('Uploading image to S3 Sever...')
                 self.upload_file(random_filename, random_filename, pic = True)
                 os.remove(random_filename)
+            except Exception as e:
+                self._logger.warn("Could not upload: %s" % e)
     def upload_timelapse(self, payload):
         enablePlugin = self.enablePlugin
         if enablePlugin:
                 path = payload['movie']
                 file_name = payload['movie_basename']
-                if os.path.getsize(path) > 1500000: #if file size > 1.5 MB, control the influx of extremely small timelapses.
+                if os.path.getsize(path) > 0: #if file size > 0 MB
                     self.upload_file(path, file_name, pic = False)
 
     @property
